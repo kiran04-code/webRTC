10cc9091e733f64fac50de8d5eb4bab04bb35009
kiran04-code <kr551344@gmail.com>
2025-06-20 17:26:48 +0530

    client

--- server/index.js
+++ server/index.js
@@ -17,9 +17,23 @@ const io = new Server(server,{
     credentials:true
  }
 })
-
+const emailToSocketIdMapping = new Map()
 io.on("connection",(Socket)=>{
     console.log("User is Connted to Socket.io:",Socket.id)
+  Socket.on("Room:join",data=>{
+    const {emailId,roomId} = data
+    emailToSocketIdMapping.set(emailId,Socket.id)
+    console.log(`${emailId} join this Room ${roomId}`)
+    Socket.join(roomId)
+    Socket.emit("joined-userstherRoom",{roomId})
+    Socket.broadcast.to(roomId).emit("joined-user",{emailId})
+  })
+
+
+
+    Socket.on("disconnect",()=>{
+        console.log("User is DissConnected")
+    })
 })
 server.listen(port,(req,res)=>{
     console.log(`server is Running on PORT http://localhost:${port}`)
